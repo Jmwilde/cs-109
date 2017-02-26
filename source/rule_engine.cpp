eccf1fd2a2b6cc5f732288f48e4d7e28d06953cd
Kaitaku Takeda <katakeda@ucsc.edu>
2017-02-25 17:41:52 -0800

    added few changes

--- source/rule_engine.cpp
+++ source/rule_engine.cpp
@@ -39,7 +39,6 @@ void RuleEngine::storeRule(string name, Rule rule)
 
 void RuleEngine::storeFact(string name, Fact fact)
 {
-	cout << "Calling storeFact.\n";
 	this->kb[name].push_back(fact);
 }
 
@@ -79,26 +78,10 @@ void RuleEngine::executeOr(Rule& rule)
 	for (int i=0; i<num_elems; i++)  // Examine each predicate
 	{
 		string predicate = rule.getPredicate(i);
-
 		cout << "Predicate " << i << ": " << predicate << endl;
 
 		// Look in the KB, then in the RB
-		//cout << rule.getName() << endl;
-		//string rule_name = rule.getName();
 		auto kb_search = this->kb.find(predicate);
-<<<<<<< HEAD
-		cout << kb_search->first;
-=======
-		//vector<Fact> v = kb_search->second;
-
-		// for (int i=0; i<v.size(); i++)
-		// {
-		// 	int num_elems = v[i].getNumPredicates();
-		// 	for (int j=0; j<num_elems; j++)
-  //   			cout << v[i].getPredicate(j) << endl;
-		// }
-
->>>>>>> origin/john
 		if( kb_search != kb.end() )
 		{
 			// Found in the KB
@@ -117,12 +100,7 @@ void RuleEngine::executeOr(Rule& rule)
 				for (int j=0; j<num_elems; j++)
     				cout << v[i].getPredicate(j) << endl;
 			}
-<<<<<<< HEAD
-		} else cout << kb_search->first << " not found in KB.\n";
-=======
-		//continue;
 		} else cout << predicate << " not found in KB.\n";
->>>>>>> origin/john
 
 		auto rb_search = this->rb.find(predicate);
 		if (rb_search != rb.end())
@@ -151,18 +129,6 @@ void RuleEngine::executeAnd(Rule& rule)
 {
 	cout << "Called executeAnd() method.\n";
 
-	return;
-}
-
-// Store the KB and RB info
-// into a text file (.sri)
-void RuleEngine::dump()
-{
-	return;
-}
-
-void RuleEngine::load()
-{
 	return;
 }
 
