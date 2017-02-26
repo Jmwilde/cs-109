940a9ebf68ce3bff6475036ef397ab534f38de25
John Wilde <johnwildefire@gmail.com>
2017-02-25 21:07:02 -0800

    Restructured executeOr and created inference function. Still testing these.

--- source/main.cpp
+++ source/main.cpp
@@ -22,11 +22,12 @@ int main(int argc, char const *argv[])
 	RuleEngine sri; // Declare a new rule engine
 
 	cout << "Welcome to the Simple Rule Inference Engine!\n";
-	cout << "Please input a command.\n";
+	cout << "Please input a command.\n\n";
 	
 	vector<string> fruits = {"Apple", "Orange", "Banana"};
 	vector<string> meats = {"Steak", "Chicken", "Pork", "Lizard"};
 	vector<string> foods = {"Fruits", "Meats"};
+	vector<string> drinks = {"Pepsi", "Mtn Dew", "Coke"};
 	vector<string> food_and_drink {"Food", "Drinks"};
 
 	//  User creats & stores some Facts
@@ -40,18 +41,17 @@ int main(int argc, char const *argv[])
 	sri.storeRule("Food", rule1);
 
 	//  Then Infers the rule!
-	sri.executeRule(rule1);
+	sri.inference("Fruits", 3);
 
-	vector<string> drinks = {"Pepsi", "Mtn Dew", "Coke"};
-	Fact fact3("Drinks", drinks);
-	sri.storeFact("Drinks", fact3);
+	// Fact fact3("Drinks", drinks);
+	// sri.storeFact("Drinks", fact3);
 
-	cout << "\nBeginning 2nd Test!\n\n";
+	// cout << "\nBeginning 2nd Test!\n\n";
 
-	Rule rule2("Food and Drink", OR, food_and_drink);
-	sri.storeRule("Food and Drink", rule2);
-	
-	sri.executeRule(rule2);
+	// Rule rule2("Food and Drink", OR, food_and_drink);
+	// sri.storeRule("Food and Drink", rule2);
+
+	// sri.executeRule(rule2);
 
 	// user inputs a command, and hits Enter/Return
 
