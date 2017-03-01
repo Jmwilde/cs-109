12301311dcab5bbd010369fc49b18f26ec749bf1
Kaitaku Takeda <katakeda@ucsc.edu>
2017-02-28 20:53:25 -0800

    merged new Makefile

---
+++ tests/test1.cpp
@@ -0,0 +1,31 @@
+// CMPS 109
+// Winter 2017
+
+// John Wilde - jmwilde
+// Nathaniel Suriawijaya - nsuriawi
+// Tako Takeda - katakeda
+// Noriaki Nakano - nnakano
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <map>
+
+#include "rule_engine.h"
+#include "fact.h"
+#include "rule.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+  RuleEngine sri; // Declare a new rule engine
+
+	cout << "Welcome to the Simple Rule Inference Engine!\n";
+	cout << "Please input a command.\n\n";
+
+  string commandLine = "";
+  cin >> commandLine;
+
+  return 0;
+}
