1ce0ea9d964de93aa63380c498c5a56484e61b2c
John Wilde <johnwildefire@gmail.com>
2017-03-10 17:17:12 -0800

    Working on fixing storeAnd() so it can deal with more complicated substitution variable patterns

--- source/rule_engine.cpp
+++ source/rule_engine.cpp
@@ -9,6 +9,8 @@
 #include "fact.h"
 #include "rule.h"
 #include "rule_engine.h"
+#include "util.h"
+#include <utility>
 #include <map>
 #include <vector>
 #include <iostream>
@@ -20,98 +22,205 @@ using namespace std;
 
 RuleEngine::RuleEngine()
 {
-	map<string, vector<Fact>> kb; // Knowledge Base
-	map<string, vector<Rule>> rb; // Rule Base
+    map<string, vector<Fact>> kb; // Knowledge Base
+    map<string, vector<Rule>> rb; // Rule Base
 }
 
 RuleEngine::~RuleEngine(){}
 
 void RuleEngine::printResults(vector<string> results)
 {
-	char letter = 'A';
-	for (int i=0; i<results.size(); i++)
-	{
-		cout << char(letter) << ":" << results[i];
-		if (i+1 != results.size()) cout << ", ";
-		letter++;
-	}
-	cout << endl;
+    char letter = 'A';
+    for (int i=0; i<results.size(); i++)
+    {
+        cout << char(letter) << ":" << results[i];
+        if (i+1 != results.size()) cout << ", ";
+        letter++;
+    }
+    cout << endl;
 }
 
 void RuleEngine::printRb(string name)
 {
-	vector<Rule> v = this->rb[name];
-	for(int i=0; i<v.size(); i++)
-	{
-		vector<string> vect = v[i].getValueVector();
-		for(int j=0; j<vect.size(); j++) cout << vect.at(j) << endl;
-	}
+    vector<Rule> v = this->rb[name];
+    for(int i=0; i<v.size(); i++)
+    {
+        vector<string> vect = v[i].getValueVector();
+        for(int j=0; j<vect.size(); j++) cout << vect.at(j) << endl;
+    }
 }
 
 bool RuleEngine::inKB(string name)
 {
-	if(kb.find(name) != kb.end()) return true;
-	else return false;
+    if(kb.find(name) != kb.end()) return true;
+    else return false;
 }
 
 bool RuleEngine::inRB(string name)
 {
-	if(rb.find(name) != rb.end()) return true;
-	else return false;
+    if(rb.find(name) != rb.end()) return true;
+    else return false;
 }
 
 void RuleEngine::storeFact(string name, vector<string> values)
 {
-	Fact new_fact(name, values);
-	if(kb.find(name) != kb.end())
-	{
-		vector<Fact> temp = kb[name];
-		for(vector<Fact>::iterator iter = temp.begin();iter != temp.end(); iter++)
-		{
-			if(iter->getValueVector() == values)
-			{
-				return;
-			}
-		}
-	}
-	this->kb[name].push_back(new_fact);
+    Fact new_fact(name, values);
+    if(kb.find(name) != kb.end())
+    {
+        vector<Fact> temp = kb[name];
+        for(vector<Fact>::iterator iter = temp.begin();iter != temp.end(); iter++)
+        {
+            if(iter->getValueVector() == values)
+            {
+                return;
+            }
+        }
+    }
+    this->kb[name].push_back(new_fact);
 }
 
-void RuleEngine::storeRule(string rule_name, logical_op_t op, vector<string> predicates)
+void RuleEngine::storeRule(string rule_name, logical_op_t op, 
+                           vector<string> predicates, 
+                           map<string,vector<string>> var_map)
 {
-	// Store rule now depends on the operator
-	if(op == OR)
-	{
-		cout << "Beginning OR storeRule\n";
-		storeOr(rule_name, predicates);
-	}else if(op == AND){
-		cout << "Beginning AND storeRule\n";
-		//storeAnd(rule_name, op, predicates);
-	}else
-		cout << "Error: No operator given!\n";
+    // Store rule now depends on the operator
+    if(op == OR)
+    {
+        cout << "Beginning OR storeRule\n";
+        storeOr(rule_name, predicates);
+    }else if(op == AND){
+        cout << "Beginning AND storeRule\n";
+        storeAnd(rule_name, op, predicates, var_map);
+    }else
+        cout << "Error: No operator given!\n";
 }
 
-// void RuleEngine::storeAnd(string rule_name, logical_op_t op, vector<string> predicates)
-// {
-// 	Should do the full execution of And like normal
-// 	Except instead of print results, it stores them under
-// 	the "rule_name" inside the RB as RULE objs
+void RuleEngine::storeAnd(string rule_name, logical_op_t op,
+                          vector<string> predicates,
+                          map<string,vector<string>> var_map, 
+                          string start_letter, string end_letter)
+{
+    vector<vector<pair<string,int>>> filter_table;
 
-// 	Also, it needs a table of predicate names & positions
-// 	so it knows what to compare for each round of filtering
-//   return;
-// }
+    // Should do the full execution of And like normal
+    // Except instead of print results, it stores them under
+    // the "rule_name" inside the RB as RULE objs
+
+    // Also, it needs a table of predicate names & positions
+    // so it knows what to compare for each round of filtering
+
+    // Step 1: Search for the start letter (and make sure the end letter is in the map too)
+    // EX: start == $A, end == $Z
+    // NOTE: This assumes there's only ONE start letter, and one end letter
+    string first_predicate = findValue(var_map, start_letter);
+    if(start == NULL){
+        cout << "Invalid AND RULE!\n";
+        return;
+    }
+    string last_predicate = findValue(var_map, end_letter);
+    if(start == NULL){
+        cout << "Invalid AND RULE!\n";
+        return;
+    }
+
+    // Step 2: For each letter associated with the first_predicate
+    int table_index = 0;
+
+    // Just call the recursion here like this instead?
+    filter_letters(first_predicate, start_letter, var_map, table_index, &filter_table);
+
+
+    // --------------------------------------------------
+    vector<string> root = var_map[first_predicate];
+    for(int i=0; i<root.size(); i++)
+    {
+        if(root[i] == start_letter) continue;  // Skip the start letter
+
+        // Search for a matching letter in the map
+        for(map<string,vector<Fact>>::iterator it = var_map.begin(); it!=var_map.end(); ++it)
+        {
+            vector<string> curr_letters = it->second;  // Get the current vector of letters
+            string current_predicate = it->first;
+
+            for(int j=0; j<curr_letters.size(); j++)
+            {
+                if(curr_letters[j] == root[i])
+                {
+                    // root[i] is a specific letter in your first predicate
+                    // If the letters match, make a pair
+                    // of the current letter's predicate and its position
+                    pair<string,int> filter(first_predicate,i)  // Name and position
+                    pair<string,int> position(current_predicate, j);
+                    
+                    // Store them in a table, where each row is a vector of these pairs
+                    filter_table[table_index].push_back(filter);
+                    filter_table[table_index].push_back(position);
+
+                    // Start a recursive filter function
+                    filter_letters(current_predicate, root[i], var_map, ++table_index, &filter_table);
+                    // That has a base case of when it can't find anymore matches
+                    // Each recursive call should ignore the 'root' first_predicate when scanning for matching letters
+                }
+            }
+        }
+    }
+    return;
+    // --------------------------------------------------------
+}
+
+RuleEngine::filter_letters(string predicate, string prev_match,
+                           map<string,vector<string>> var_map, int index, 
+                           &vector<vector<pair<string,int>>> filter_table)
+{
+    // Filter should look at the current predicate's letters
+    vector<string> filter_letters = var_map[predicate];  // Vector associated with the predicate
+
+    for(int i=0; i<filter_letters.size(); i++)
+    {
+        if(filter_letters[i] == prev_match) continue;  // Skip the previously matched letter
+
+        // Search for a matching letter in the map of predicates-to-letters (var_map)
+        for(map<string,vector<Fact>>::iterator it = var_map.begin(); it!=var_map.end(); ++it)
+        {
+            string current_predicate = it->first;  // Current predicate
+            vector<string> curr_letters = it->second;  // Get the current vector of letters
+
+            for(int j=0; j<curr_letters.size(); j++)
+            {
+                if(filter_letters[i] == curr_letters[j])
+                {
+                    // A match is found!
+                    // Store it in the table
+
+                    // Name and position stored as pairs
+                    pair<string,int> filter(predicate,i);
+                    pair<string,int> position(current_predicate, j);
+
+                    // Store them in a table, where each row is a vector of these pairs
+                    filter_table[table_index].push_back(filter);
+                    filter_table[table_index].push_back(position);
+
+                    // Call it again for the next round of finding matching letters!
+                    filter_letters(current_predicate, filter_letters[i], var_map, ++table_index, &filter_table);
+                }
+            }
+        }
+    // In the base case we did not find any matching letters
+    // Set the current row of the filter table to NULL
+    filter_table[table_index] == NULL;
+    return;
+}
 
 void RuleEngine::storeOr(string rule_name, vector<string> predicates)
 {
-	// NOTE: Could replace this for loop with parallel threads!
+    // NOTE: Could replace this for loop with parallel threads!
 
-	// For each predicate name
-	for(int i=0; i<predicates.size(); i++)
-	{
-		cout << "Storing values from " << predicates[i] << "\n";
-		storeValues(rule_name, OR, predicates, predicates[i]);
-	}
+    // For each predicate name
+    for(int i=0; i<predicates.size(); i++)
+    {
+        cout << "Storing values from " << predicates[i] << "\n";
+        storeValues(rule_name, OR, predicates, predicates[i]);
+    }
 }
 
 // NOTE: Does not check for # of parameters when storing
@@ -120,90 +229,90 @@ void RuleEngine::storeOr(string rule_name, vector<string> predicates)
 
 void RuleEngine::storeValues(string rule_name, logical_op_t op, vector<string> predicate_names, string predicate)
 {
-	if(inRB(predicate))
-	{
-		// Get the vector of RULES associated with the predicate/key
-		vector<Rule> rule_vect = rb[predicate];
+    if(inRB(predicate))
+    {
+        // Get the vector of RULES associated with the predicate/key
+        vector<Rule> rule_vect = rb[predicate];
 
-		// For each RULE
-		for (int i=0; i<rule_vect.size(); i++)
-		{
-			// Create a new rule using the current rule's values
-			Rule new_rule(rule_name, op, predicate_names, rule_vect[i].getValueVector());
+        // For each RULE
+        for (int i=0; i<rule_vect.size(); i++)
+        {
+            // Create a new rule using the current rule's values
+            Rule new_rule(rule_name, op, predicate_names, rule_vect[i].getValueVector());
 
-			// Store it in the RB
-			rb[rule_name].push_back(new_rule);
-		}
-	}
+            // Store it in the RB
+            rb[rule_name].push_back(new_rule);
+        }
+    }
 
-	if(inKB(predicate))
-	{
-		vector<Fact> fact_vect = kb[predicate];
+    if(inKB(predicate))
+    {
+        vector<Fact> fact_vect = kb[predicate];
 
-		// For each FACT
-		for (int i=0; i<fact_vect.size(); i++)
-		{
-			Rule new_rule(rule_name, op, predicate_names, fact_vect[i].getValueVector());  // Create a new rule
-			rb[rule_name].push_back(new_rule);  // Store it in the RB
-		}
-	}
+        // For each FACT
+        for (int i=0; i<fact_vect.size(); i++)
+        {
+            Rule new_rule(rule_name, op, predicate_names, fact_vect[i].getValueVector());  // Create a new rule
+            rb[rule_name].push_back(new_rule);  // Store it in the RB
+        }
+    }
 }
 
 void RuleEngine::parseInput(string commandLine)
 {
-	string name = "";
-	string query = "";
-	string stringOp = "";
-  	string pred = "";
-  	string sriFile = "";
-  	string temp = "";
-	int count = 1;
-	logical_op_t op;
-	vector<string> paramVec;
-	vector<string> predVec;
-  	stringstream iss(commandLine);
+    string name = "";
+    string query = "";
+    string stringOp = "";
+    string pred = "";
+    string sriFile = "";
+    string temp = "";
+    int count = 1;
+    logical_op_t op;
+    vector<string> paramVec;
+    vector<string> predVec;
+    stringstream iss(commandLine);
 
-  	getline(iss, name, ' ');
+    getline(iss, name, ' ');
 
-  	if(name == "FACT"){
-    	try{
-      		if(commandLine.find('(') == -1) throw 0;
-      		if(commandLine.find(')') == -1) throw 0;
-    	}catch(int e){
-      	cout << "Error: Invalid command line argument" << endl;
-    	}
-    	getline(iss, query, '(');
-   	 	while(getline(iss, temp, ',')){
-      	if(temp.back() == ')') temp.pop_back();
-      	paramVec.push_back(temp);
-    	}
-	this->storeFact(query, paramVec);
+    if(name == "FACT"){
+        try{
+            if(commandLine.find('(') == -1) throw 0;
+            if(commandLine.find(')') == -1) throw 0;
+        }catch(int e){
+        cout << "Error: Invalid command line argument" << endl;
+        }
+        getline(iss, query, '(');
+        while(getline(iss, temp, ',')){
+        if(temp.back() == ')') temp.pop_back();
+        paramVec.push_back(temp);
+        }
+    this->storeFact(query, paramVec);
 
-  	}else if(name == "RULE"){
-    	try{
-      		if(commandLine.find(":-") == -1) throw 0;
-    	}catch(int e){
-     		cout << "Error: Invalid command line argument" << endl;
-    	}
-    	getline(iss, query, '(');
-    	getline(iss, temp, ' ');
-    	getline(iss, stringOp, ' ');
+    }else if(name == "RULE"){
+        try{
+            if(commandLine.find(":-") == -1) throw 0;
+        }catch(int e){
+            cout << "Error: Invalid command line argument" << endl;
+        }
+        getline(iss, query, '(');
+        getline(iss, temp, ' ');
+        getline(iss, stringOp, ' ');
 
-    	if(stringOp == "OR") op = OR;
-		else if(stringOp == "AND") op = AND;
-		else cout << "Error: Invalid command line argument" << endl;
+        if(stringOp == "OR") op = OR;
+        else if(stringOp == "AND") op = AND;
+        else cout << "Error: Invalid command line argument" << endl;
 
-    	while(getline(iss, pred, '(')){
-      		predVec.push_back(pred);
-      		iss.putback('(');
-      		getline(iss, temp, ' ');
-      		try{
-        		if(temp.find('(') == -1 || temp.find(')') == -1) throw 0;
-      		}catch(int e){
-        		cout << "Error: Invalid command line argument" << endl;
-      		}
-    	}
-		this->storeRule(query, op, predVec);
+        while(getline(iss, pred, '(')){
+            predVec.push_back(pred);
+            iss.putback('(');
+            getline(iss, temp, ' ');
+            try{
+                if(temp.find('(') == -1 || temp.find(')') == -1) throw 0;
+            }catch(int e){
+                cout << "Error: Invalid command line argument" << endl;
+            }
+        }
+        this->storeRule(query, op, predVec);
   }else if(name == "INFERENCE"){
     try{
       if(commandLine.find('(') == -1) throw 0;
@@ -212,91 +321,91 @@ void RuleEngine::parseInput(string commandLine)
       cout << "Error: Invalid command line argument" << endl;
     }
     getline(iss, query, '(');
-		getline(iss, temp, ')');
-		for(int i=0; i<temp.size(); i++){
-			if(temp.at(i) == ',') count++;
-		}
-		getline(iss, temp, ' ');
-		getline(iss, pred);
-		if(pred != ""){
-			this->inference(query, count, pred);
-		}
-		else{
-			this->inference(query, count);
-		}
+        getline(iss, temp, ')');
+        for(int i=0; i<temp.size(); i++){
+            if(temp.at(i) == ',') count++;
+        }
+        getline(iss, temp, ' ');
+        getline(iss, pred);
+        if(pred != ""){
+            this->inference(query, count, pred);
+        }
+        else{
+            this->inference(query, count);
+        }
   }else if(name == "LOAD"){
-  	cout << "Called LOAD!\n";
+    cout << "Called LOAD!\n";
     getline(iss, sriFile);
-		this->load(sriFile);
+        this->load(sriFile);
   }else if(name == "DUMP"){
     getline(iss, sriFile);
-		this->dump(sriFile);
+        this->dump(sriFile);
   }else if(name == "DROP"){
     getline(iss, pred);
-		this->drop(pred);
-  	}else{
-		cout << "\nError: Invalid command line argument" << endl;
-	}
-  	return;
+        this->drop(pred);
+    }else{
+        cout << "\nError: Invalid command line argument" << endl;
+    }
+    return;
 }
 
 void RuleEngine::inference(string query, int num_sub_vars, string name)
 {
-	searchKnowledgeBase(query, num_sub_vars, true, name);
-	searchRuleBase(query, num_sub_vars, true, name);
+    searchKnowledgeBase(query, num_sub_vars, true, name);
+    searchRuleBase(query, num_sub_vars, true, name);
 }
 
 void RuleEngine::inference(string query, int num_sub_vars)
 {
-	searchKnowledgeBase(query, num_sub_vars, false, "");
-	searchRuleBase(query, num_sub_vars, false, "");
+    searchKnowledgeBase(query, num_sub_vars, false, "");
+    searchRuleBase(query, num_sub_vars, false, "");
 }
 
 void RuleEngine::searchKnowledgeBase(string query, int num_values, bool add, string name)
 {
-	//  Look in the KB
-	auto kb_search = this->kb.find(query);
-	if(kb_search != kb.end())
-	{
-		// Found in the KB
-		vector<Fact> fact_vect = kb[query];
+    //  Look in the KB
+    auto kb_search = this->kb.find(query);
+    if(kb_search != kb.end())
+    {
+        // Found in the KB
+        vector<Fact> fact_vect = kb[query];
 
-		// For each Fact in the vector
-		for (int i=0; i<fact_vect.size(); i++)
-		{
-			// Check if the current Fact has the correct # of values
-			if (fact_vect[i].getNumValues() != num_values) continue;
+        // For each Fact in the vector
+        for (int i=0; i<fact_vect.size(); i++)
+        {
+            // Check if the current Fact has the correct # of values
+            if (fact_vect[i].getNumValues() != num_values) continue;
 
-			// Print corresponding list of strings
-			std::vector<string> results = fact_vect[i].getValueVector();
-			printResults(results);
+            // Print corresponding list of strings
+            std::vector<string> results = fact_vect[i].getValueVector();
+            printResults(results);
 
-			if(add) storeFact(name, results);
-		}
-	}
+            if(add) storeFact(name, results);
+        }
+    }
 }
 
 void RuleEngine::searchRuleBase(string query, int num_values, bool add, string name)
 {
-	//  Look in the RB
-	if(inRB(query))
-	{
-		// Found in the RB
-		vector<Rule> rule_vect = rb[query];
+    //  Look in the RB
+    if(inRB(query))
+    {
+        // Found in the RB
+        vector<Rule> rule_vect = rb[query];
 
-		// For each Rule in the vector
-		for (int i=0; i<rule_vect.size(); i++)
-		{
-			// Check if the current Rule has the correct # of values
-			if (rule_vect[i].getNumValues() != num_values) continue;
+        // For each Rule in the vector
+        for (int i=0; i<rule_vect.size(); i++)
+        {
+            // Check if the current Rule has the correct # of values
+            if (rule_vect[i].getNumValues() != num_values) continue;
 
-			// Print corresponding list of strings
-			vector<string> results = rule_vect[i].getValueVector();
-			printResults(results);
+            // Print corresponding list of strings
+            vector<string> results = rule_vect[i].getValueVector();
+            printResults(results);
 
-			if(add) storeFact(name, results);
-		}
-	}
+            if(add) storeFact(name, results);
+        }
+    }
 }
 
 void RuleEngine::drop(string input)
--- tests/test.cpp
+++ tests/test.cpp
@@ -35,6 +35,35 @@ void testStoreRule()
 	sri.printRb("Parent");
 }
 
+void testStoreAnd()
+{
+	RuleEngine sri;
+
+	// Setup a mapping of each of the rule's predicates
+	// to it's respective substituion variables
+	string begin = "A";
+	string end = "Z";
+
+	// This will normally be generated via the parser
+	// And passed to the storeAnd() function
+	map<string, vector<string>> variables_map;
+	variables_map["Apples"] = {"X", "A", "B"};
+	variables_map["Oranges"] = {"Q", "B", "H"};
+	variables_map["Bananas"] = {"H", "Z"};
+
+	vector<string> str1 = {"Red", "Green", "Pink"};
+	sri.storeFact("Apples", str1);
+
+	vector<string> str2 = {"Orange", "Pink", "Yellow"};
+	sri.storeFact("Oranges", str2);
+
+	vector<string> str3 = {"Yellow", "Green"};
+	sri.storeFact("Bananas", str3);
+
+	vector<string> preds = {"Apples", "Oranges", "Bananas"};
+	sri.storeRule("Fruits", AND, preds);
+}
+
 int main()
 {
 	cout << "*** Running test file! ***\n";
